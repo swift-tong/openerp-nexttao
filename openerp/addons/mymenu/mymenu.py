6c748b18e99656ebc3023fdec51f73e6f21e90f9
weiqing.tong <weiqing.tong@nexttao.com>
2021-03-24 19:59:31 +0800

    4

--- openerp/addons/mymenu/mymenu.py
+++ openerp/addons/mymenu/mymenu.py
@@ -1,7 +1,7 @@
 from openerp.osv import osv, fields
 
 
-class mymenu_mymenu(osv.Model):
+class mymenu_mymenu(osv.osv):
     _name = 'mymenu.mymenu'
     _columns = {
         'x_daterequired': fields.date('Date Required', required=True),
