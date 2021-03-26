b23044f928a9d55ca31ac0db6e41579fbcd7e24a
weiqing.tong <weiqing.tong@nexttao.com>
2021-03-26 20:22:03 +0800

    222

--- openerp/addons/re_expense/re_expense.py
+++ openerp/addons/re_expense/re_expense.py
@@ -28,29 +28,29 @@ class re_expense_expense(osv.osv):
     _order = "id desc"
 
     _columns = {
-        'user': fields.many2one('res.users', '员工', required=True),
-        'date': fields.date('日期', select=True, readonly=True,states={'draft': [('readonly', False)]}),
-        'department': fields.text('部门', readonly=True,states={'draft': [('readonly', False)]}),
-        'note': fields.text('说明', readonly=True,states={'submitted': [('readonly', False)]}),
-        'total_amount': fields.function(_amount, readonly=True,string='总金额'),
+        'user': fields.many2one('res.users', u'员工', required=True),
+        'date': fields.date(u'日期', select=True, readonly=True,states={'draft': [('readonly', False)]}),
+        'department': fields.text(u'部门', readonly=True,states={'draft': [('readonly', False)]}),
+        'note': fields.text(u'说明', readonly=True,states={'submitted': [('readonly', False)]}),
+        'total_amount': fields.function(_amount, readonly=True,string=u'总金额'),
 
-        'user_create': fields.many2one('res.users', '创建人', required=True),
-        'date_create': fields.datetime('创建时间',  readonly=True),
+        'user_create': fields.many2one('res.users', u'创建人', required=True),
+        'date_create': fields.datetime(u'创建时间',  readonly=True),
 
-        'user_check': fields.many2one('res.users', '审核人', required=True),
-        'date_check': fields.datetime('审核时间',  readonly=True),
+        'user_check': fields.many2one('res.users', u'审核人', required=True),
+        'date_check': fields.datetime(u'审核时间',  readonly=True),
 
-        'user_cancel': fields.many2one('res.users', '取消人', required=True),
-        'date_cancel': fields.datetime('取消时间', readonly=True),
+        'user_cancel': fields.many2one('res.users', u'取消人', required=True),
+        'date_cancel': fields.datetime(u'取消时间', readonly=True),
 
-        'user_commit': fields.many2one('res.users', '提交人', required=True),
-        'date_commit': fields.datetime('提交时间', readonly=True),
+        'user_commit': fields.many2one('res.users', u'提交人', required=True),
+        'date_commit': fields.datetime(u'提交时间', readonly=True),
 
         'state': fields.selection([
-            ('draft', '新建'),
-            ('submitted', '已提交'),
-            ('done', '已完成'),
-            ('cancelled', '已取消'),
+            ('draft', u'新建'),
+            ('submitted', u'已提交'),
+            ('done', u'已完成'),
+            ('cancelled', u'已取消'),
             ],
             'Status', readonly=True, track_visibility='onchange',
         ),
@@ -79,14 +79,14 @@ class re_expense_line(osv.osv):
 
     _columns = {
         'expense_id': fields.many2one('re.expense.expense', 'Expense'),
-        'product_id': fields.many2one('product.product', '产品',readonly=True, states={'draft': [('readonly', False)]}),
-        'product_amount': fields.integer('产品数量',readonly=True, states={'draft': [('readonly', False)]}),
-        'expense_data': fields.date('费用日期', required=True, readonly=True, states={'draft': [('readonly', False)]}),
-        'expense_note': fields.text('费用备注', required=False,readonly=True, states={'draft': [('readonly', False)]}),
-        'order_no.': fields.text('单号', required=False,readonly=True, states={'draft': [('readonly', False)]}),
-        'auxiliary.': fields.text('辅助核算项', required=False,readonly=True, states={'draft': [('readonly', False)]}),
-        'amount': fields.integer(string='金额'),
-        'total_amount': fields.function(_amount, string='合计', digits_compute=dp.get_precision('Account')),
+        'product_id': fields.many2one('product.product', u'产品',readonly=True, states={'draft': [('readonly', False)]}),
+        'product_amount': fields.integer(u'产品数量',readonly=True, states={'draft': [('readonly', False)]}),
+        'expense_data': fields.date(u'费用日期', required=True, readonly=True, states={'draft': [('readonly', False)]}),
+        'expense_note': fields.text(u'费用备注', required=False,readonly=True, states={'draft': [('readonly', False)]}),
+        'order_no.': fields.text(u'单号', required=False,readonly=True, states={'draft': [('readonly', False)]}),
+        'auxiliary.': fields.text(u'辅助核算项', required=False,readonly=True, states={'draft': [('readonly', False)]}),
+        'amount': fields.integer(string=u'金额'),
+        'total_amount': fields.function(_amount, string=u'合计', digits_compute=dp.get_precision('Account')),
         }
 
     _defaults = {
