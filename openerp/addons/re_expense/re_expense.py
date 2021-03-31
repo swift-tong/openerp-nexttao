ca4420602d4f5a9fe378ddaf7ba094b575c23a55
weiqing.tong <weiqing.tong@nexttao.com>
2021-03-31 17:02:01 +0800

    1

--- openerp/addons/re_expense/re_expense.py
+++ openerp/addons/re_expense/re_expense.py
@@ -44,7 +44,7 @@ class re_expense_expense(osv.osv):
         'department': fields.many2one('hr.department', u'部门', readonly=True, states={'draft': [('readonly', False)]}),
         'instructions': fields.char(u'说明', readonly=True, states={'draft': [('readonly', False)]}),
         'total_amount': fields.function(_amount, readonly=True, string=u'总金额'),
-        'reception': fields.boolean(_check_role, readonly=True,string=u'已收单'),
+        'reception': fields.function(_check_role, readonly=True,string=u'已收单'),
         'note': fields.text(u'备注', readonly=True,
                             states={'draft': [('readonly', False)], 'submitted': [('readonly', False)]}),
 
