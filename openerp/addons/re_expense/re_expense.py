c321201ba5bd7a894fa78fe5d4391a438fdc271e
weiqing.tong <weiqing.tong@nexttao.com>
2021-03-31 20:37:26 +0800

    1

--- openerp/addons/re_expense/re_expense.py
+++ openerp/addons/re_expense/re_expense.py
@@ -30,13 +30,13 @@ class re_expense_expense(osv.osv):
     def _check_role(self,cr,uid,ids, field_name, arg, context=None):
         res = {}
         for expense in self.browse(cr, uid, ids, context=context):
-            reception = expense.reception
+            # reception = expense.reception
             if self.pool.get('res.users').has_group(cr, uid, "re_expense.expense_users"):
-                self._readonly = True
-                res[expense.id] = reception
+                # self._readonly = True
+                res[expense.id] = True
             elif self.pool.get('res.users').has_group(cr, uid, "re_expense.expense_manager"):
-                res[expense.id] = reception
-                self._readonly = False
+                res[expense.id] = False
+                # self._readonly = False
         return res
 
     _name = 're.expense.expense'
@@ -50,8 +50,8 @@ class re_expense_expense(osv.osv):
         'department': fields.many2one('hr.department', u'部门', readonly=True, states={'draft': [('readonly', False)]}),
         'instructions': fields.char(u'说明', readonly=True, states={'draft': [('readonly', False)]}),
         'total_amount': fields.function(_amount, readonly=True, string=u'总金额', digits=(12,3)),
-        # 'reception': fields.boolean(u'已收单', readonly=_readonly, states={'submitted': [('readonly', False)]}),
-        'reception': fields.function(_check_role,string=u'已收单', readonly=_readonly),
+        'reception': fields.boolean(u'已收单', readonly=_readonly, states={'submitted': [('readonly', False)]}),
+        # 'reception': fields.function(_check_role,string=u'已收单', readonly=_readonly),
         'note': fields.text(u'备注', readonly=True,
                             states={'draft': [('readonly', False)], 'submitted': [('readonly', False)]}),
 
