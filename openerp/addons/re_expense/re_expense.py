7650bb2757d249e4e3b5f09ab49fde9784f0a4d1
weiqing.tong <weiqing.tong@nexttao.com>
2021-03-31 20:00:46 +0800

    1

--- openerp/addons/re_expense/re_expense.py
+++ openerp/addons/re_expense/re_expense.py
@@ -20,11 +20,6 @@ class re_expense_expense(osv.osv):
 
 
     def _amount(self, cr, uid, ids, field_name, arg, context=None):
-        if self.pool.get('res.users').has_group(cr, uid, "re_expense.expense_users"):
-            _readonly = True
-        elif self.pool.get('res.users').has_group(cr, uid, "re_expense.expense_manager"):
-            _readonly = False
-        
         res = {}
         for expense in self.browse(cr, uid, ids, context=context):
             total = 0.0
@@ -34,14 +29,15 @@ class re_expense_expense(osv.osv):
         return res
 
     def _check_role(self,cr,uid,ids, field_name, arg, context=None):
+        pro_info = self.read(cr,uid,ids,["reception"])
         res = {}
         for expense in self.browse(cr, uid, ids, context=context):
             if self.pool.get('res.users').has_group(cr, uid, "re_expense.expense_users"):
-                # self._readonly = True
-                res[expense.id] = 1
+                self._readonly = True
+                res[expense.id] = pro_info.get('reception')
             elif self.pool.get('res.users').has_group(cr, uid, "re_expense.expense_manager"):
-                res[expense.id] = 0
-                # self._readonly = False
+                res[expense.id] = pro_info.get('reception')
+                self._readonly = False
         return res
 
     _name = 're.expense.expense'
@@ -55,8 +51,8 @@ class re_expense_expense(osv.osv):
         'department': fields.many2one('hr.department', u'部门', readonly=True, states={'draft': [('readonly', False)]}),
         'instructions': fields.char(u'说明', readonly=True, states={'draft': [('readonly', False)]}),
         'total_amount': fields.function(_amount, readonly=True, string=u'总金额', digits=(12,3)),
-        'reception': fields.boolean(u'已收单', readonly=_readonly, states={'submitted': [('readonly', False)]}),
-        # 'reception': fields.function(_check_role,string=u'已收单'),
+        # 'reception': fields.boolean(u'已收单', readonly=_readonly, states={'submitted': [('readonly', False)]}),
+        'reception': fields.function(_check_role,string=u'已收单', readonly=_readonly),
         'note': fields.text(u'备注', readonly=True,
                             states={'draft': [('readonly', False)], 'submitted': [('readonly', False)]}),
 
