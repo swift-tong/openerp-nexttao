c7b33c6385d517bad136486a7c296ce5eb662ebb
weiqing.tong <weiqing.tong@nexttao.com>
2021-03-31 19:41:34 +0800

    1

--- openerp/addons/re_expense/__openerp__.py
+++ openerp/addons/re_expense/__openerp__.py
@@ -31,10 +31,10 @@
     'website': 'http://www.openerp.com',
     'depends': ['base','account_voucher', 'account_accountant'],
     'data': [
-        're_expense_view.xml',
-        're_expense_workflow.xml',
         'security/security.xml',
         'security/ir.model.access.csv',
+        're_expense_view.xml',
+        're_expense_workflow.xml',
     ],
     'installable': True,
     'auto_install': False,
