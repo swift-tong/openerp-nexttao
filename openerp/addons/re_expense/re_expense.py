b9723cbc3b886600240bad8b0d5153268d1c7fb4
weiqing.tong <weiqing.tong@nexttao.com>
2021-03-31 18:06:35 +0800

    1

--- openerp/addons/re_expense/re_expense.py
+++ openerp/addons/re_expense/re_expense.py
@@ -30,7 +30,7 @@ class re_expense_expense(osv.osv):
     def _check_role(self,cr,uid,ids, field_name, arg, context=None):
 
         user_obj = self.pool.get('res.users')
-        group = user_obj.read(cr,uid,ids,['login'])
+        group = user_obj.read(cr,uid,ids,1)
         print(group)
         return {}
 
